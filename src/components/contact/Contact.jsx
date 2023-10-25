edb9e152be18a6ae2c6a7d2668792759dd420614
Sugan T <t.suganeeshwaran@gmail.com>
2023-10-25 14:03:28 +0530

    form submitting issues

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,10 +3,14 @@ import "./contact.scss";
 
 function Contact({ setMenuOpen }) {
   const [Message, setMessageUpdate] = useState(false);
+  const [Email, setEmail] = useState("");
+  const [Textarea, setTextarea] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     setMessageUpdate(true);
+    setEmail("");
+    setTextarea("");
   };
 
   return (
@@ -17,10 +21,24 @@ function Contact({ setMenuOpen }) {
       <div className="right">
         <h2>Contact</h2>
         <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Email.com" />
-          <textarea name="" id="" placeholder="Message"></textarea>
+          <input
+            type="text"
+            placeholder="Email.com"
+            value={Email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <textarea
+            name=""
+            id=""
+            placeholder="Message"
+            value={Textarea}
+            onChange={(e) => setTextarea(e.target.value)}
+          ></textarea>
+
           {!Message ? (
-            <button type="submit">Send</button>
+            <button type="submit" onClick={handleSubmit}>
+              Send
+            </button>
           ) : (
             <span>Thank's, I'll get back to you ASAP ;)</span>
           )}
